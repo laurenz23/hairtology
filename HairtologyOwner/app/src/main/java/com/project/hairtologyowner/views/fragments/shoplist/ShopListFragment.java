51add80622249d2fb68aef0f9a05878ac08a061f
LMG <laurenzgonz23@gmail.com>
2023-09-20 09:18:50 +0800

    WIP: Added shop list view data

---
+++ HairtologyOwner/app/src/main/java/com/project/hairtologyowner/views/fragments/shoplist/ShopListFragment.java
@@ -0,0 +1,37 @@
+package com.project.hairtologyowner.views.fragments.shoplist;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.project.hairtologyowner.R;
+
+public class ShopListFragment extends Fragment {
+
+    private ShopListViewModel mViewModel;
+
+    public static ShopListFragment newInstance() {
+        return new ShopListFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_shop_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(ShopListViewModel.class);
+    }
+
+}
---
+++ HairtologyOwner/app/src/main/java/com/project/hairtologyowner/views/fragments/shoplist/ShopListViewModel.java
@@ -0,0 +1,7 @@
+package com.project.hairtologyowner.views.fragments.shoplist;
+
+import androidx.lifecycle.ViewModel;
+
+public class ShopListViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}

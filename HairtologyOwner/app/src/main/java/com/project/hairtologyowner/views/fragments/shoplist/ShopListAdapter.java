18207c4c7b64880461fea298fde28efe5b5b58d2
LMG <laurenzgonz23@gmail.com>
2023-09-20 09:50:09 +0800

    WIP: Working on shop info

---
+++ HairtologyOwner/app/src/main/java/com/project/hairtologyowner/views/fragments/shoplist/ShopListAdapter.java
@@ -0,0 +1,87 @@
+package com.project.hairtologyowner.views.fragments.shoplist;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.project.hairtologyowner.R;
+import com.project.hairtologyowner.models.ShopModel;
+
+import java.util.ArrayList;
+
+public class ShopListAdapter extends RecyclerView.Adapter<ShopListAdapter.ViewHolder> {
+
+    public interface OnShopItemListener {
+        void onTap(int position, ShopModel shop);
+    }
+
+    private Context mContext;
+    private ArrayList<ShopModel> mShopArrayList;
+    private ShopListAdapter.OnShopItemListener mListener;
+
+    public ShopListAdapter(Context context, ArrayList<ShopModel> shopArrayList) {
+        mContext = context;
+        mShopArrayList = shopArrayList;
+    }
+
+    public void setOnShopItemListener(OnShopItemListener listener) {
+        mListener = listener;
+    }
+
+    @NonNull
+    @Override
+    public ShopListAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.list_item_shop_layout, parent, false);
+        return new ViewHolder(view, mListener);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ShopListAdapter.ViewHolder holder, int position) {
+        ShopModel shop = mShopArrayList.get(position);
+        holder.shop = shop;
+        holder.name.setText(shop.getShopInfo().getName());
+        holder.address.setText(shop.getShopInfo().getAddress());
+        holder.schedules.setText(shop.getShopInfo().getHour());
+        holder.price.setText(shop.getShopInfo().getPrice());
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mShopArrayList == null) {
+            return 0;
+        }
+
+        return mShopArrayList.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        private ShopModel shop;
+        private LinearLayout shopLinearLayout;
+        private TextView name;
+        private TextView address;
+        private TextView schedules;
+        private TextView price;
+
+        public ViewHolder(@NonNull View itemView, OnShopItemListener listener) {
+            super(itemView);
+
+            shopLinearLayout = itemView.findViewById(R.id.itemShopLinearLayout);
+            name = itemView.findViewById(R.id.itemShopName);
+            address = itemView.findViewById(R.id.itemShopAddress);
+            schedules = itemView.findViewById(R.id.itemShopSchedules);
+            price = itemView.findViewById(R.id.itemShopPrice);
+
+            shopLinearLayout.setOnClickListener(v -> {
+                listener.onTap(getAdapterPosition(), shop);
+            });
+        }
+    }
+
+}
--- HairtologyOwner/app/src/main/java/com/project/hairtologyowner/views/fragments/shoplist/ShopListFragment.java
+++ HairtologyOwner/app/src/main/java/com/project/hairtologyowner/views/fragments/shoplist/ShopListFragment.java
@@ -2,21 +2,30 @@ package com.project.hairtologyowner.views.fragments.shoplist;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.project.hairtologyowner.R;
+import com.project.hairtologyowner.models.ShopInfo;
+import com.project.hairtologyowner.models.ShopModel;
+
+import java.util.ArrayList;
 
 public class ShopListFragment extends Fragment {
 
     private ShopListViewModel mViewModel;
+    private ShopListAdapter mShopListAdapter;
+    private ArrayList<ShopModel> mShopArrayList = new ArrayList<>();
 
     public static ShopListFragment newInstance() {
         return new ShopListFragment();
@@ -25,13 +34,50 @@ public class ShopListFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_shop_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_shop_list, container, false);
+
+        mShopListAdapter = new ShopListAdapter(getContext(), mShopArrayList);
+
+        RecyclerView recyclerView = view.findViewById(R.id.shopListRecyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(mShopListAdapter);
+
+        return view;
     }
 
+
+    @SuppressLint("NotifyDataSetChanged")
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(ShopListViewModel.class);
+
+        ShopInfo shopInfo1 = new ShopInfo();
+        shopInfo1.setName("Shop 1");
+        shopInfo1.setAddress("Address 1");
+        shopInfo1.setDescription("Description 1");
+        shopInfo1.setHour("Hour 1");
+        shopInfo1.setId(1);
+        shopInfo1.setPrice("Price 1");
+
+        ShopModel shopModel1 = new ShopModel();
+        shopModel1.setShopInfo(shopInfo1);
+
+        ShopInfo shopInfo2 = new ShopInfo();
+        shopInfo2.setName("Shop 2");
+        shopInfo2.setAddress("Address 2");
+        shopInfo2.setDescription("Description 2");
+        shopInfo2.setHour("Hour 2");
+        shopInfo2.setId(2);
+        shopInfo2.setPrice("Price 2");
+
+        ShopModel shopModel2 = new ShopModel();
+        shopModel2.setShopInfo(shopInfo2);
+
+        mShopArrayList.add(shopModel1);
+        mShopArrayList.add(shopModel2);
+
+        mShopListAdapter.notifyDataSetChanged();
     }
 
 }
